4ce0e0399fab521f5de42aa2fd09e5b04370a40d
RodolpheMrts <rodolphe.martischang@reseau.eseo.fr>
2024-11-16 15:16:17 +0100

    correction de la merde de vautrin

--- Test_API.py
+++ Test_API.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -6,11 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-commune = os.getenv('COMMUNE')
-latitude = os.getenv('LATITUDE')
-longitude = os.getenv('LONGITUDE')
-
-requete_gravite = f'https://api.vigieau.gouv.fr/api/zones?lon={longitude}&lat={latitude}&commune={commune}'
+requete_gravite = os.getenv('URL_API_GRAVITE')
 requete_nappes = os.getenv('URL_API_NAPPES')
 requete_riviere = os.getenv('URL_API_RIVIERES')
 requete_meteo = os.getenv('URL_API_METEO')
@@ -66,7 +63,38 @@ def get_nappes():
 def get_riviere():
     return get_request(requete_riviere)
 
+def get_meteo():
+    response = get_request(requete_meteo)
+    if "hourly" not in response:
+        return response
+
+    hourly_data = response["hourly"]
+    times = hourly_data.get("time", [])
+    temperatures = hourly_data.get("temperature_2m", [])
+    humidities = hourly_data.get("relative_humidity_2m", [])
+    precipitations = hourly_data.get("precipitation", [])
+
+    grouped_data = []
+    for i in range(len(times)):
+        temperature = temperatures[i] if i < len(temperatures) else None
+        humidity = humidities[i] if i < len(humidities) else None
+        precipitation = precipitations[i] if i < len(precipitations) else None
+
+        if temperature is not None or humidity is not None or precipitation is not None:
+            grouped_data.append({
+                "time": times[i],
+                "temperature": temperature,
+                "humidity": humidity,
+                "precipitation": precipitation
+            })
+
+    response["hourly"] = grouped_data
+    return response
+
+
+
 
 print(get_nappes())
 print(get_riviere())
 print(create_dict_gravite(get_gravite(), get_date_debut(), get_date_fin()))
+print(json.dumps(get_meteo(), indent=2))
